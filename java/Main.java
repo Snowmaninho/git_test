6f13127bd4b61c80c5964d01f3d7dd3b14b959f2
Sergey Antonov <55987825+Snowmaninho@users.noreply.github.com>
2022-03-04 11:21:07 +0300

    Update Main from Git

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is first commit line");
+        System.out.println("This changes from gitHub");
     }
 }
