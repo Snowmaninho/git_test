09291e189750b3ec3db229c056246ded9fffede0
Snowman <icemaninho@yandex.ru>
2022-03-04 12:15:37 +0300

    revert changes

---
+++ java/feature/Feature.java
@@ -0,0 +1,22 @@
+public class Feature {
+    public int id;
+
+    public Feature(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Feature{" +
+                "id=" + id +
+                '}';
+    }
+}
